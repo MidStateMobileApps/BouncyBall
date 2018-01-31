5a9195e2bb77a91b5dd5f0995f2667ee124912b1
Biedenbender <16269525@mstc.edu>
2018-01-30 19:04:20 -0600

    Added Game view and Game scene

---
+++ BouncyBall/GameLayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CocosSharp;
+
+namespace BouncyBall
+{
+    public class GameLayer :CCLayerColor
+    {
+        CCSprite paddleSprite;
+
+        CCSprite ballSprite;
+
+        CCLabel scoreLabel;
+
+        CCLabel playLabel;
+
+        float ballXVelocity;
+        float ballYVelocity;
+
+        const float gravity = 140;
+
+        int levelMultiplier = 1;
+
+        int score = 0;
+
+        public GameLayer():base(CCColor4B.Black)
+        {
+            paddleSprite = new CCSprite("paddle");
+            paddleSprite.PositionX = 100;
+            paddleSprite.PositionY = 100;
+            AddChild(paddleSprite);
+
+            ballSprite = new CCSprite("ball");
+            ballSprite.PositionX = 320;
+            ballSprite.PositionY = 600;
+            AddChild(ballSprite);
+        }
+    }
+}
--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -3,6 +3,7 @@ using Android.Widget;
 using Android.OS;
 using Android;
 using CocosSharp;
+using System.Collections.Generic;
 
 namespace BouncyBall.Droid
 {
@@ -18,11 +19,42 @@ namespace BouncyBall.Droid
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.myButton);
+            CCGameView gameView = (CCGameView)FindViewById(Resource.Id.GameView);
 
-            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+            gameView.ViewCreated += LoadGame;
+            
+        }
+
+        private void LoadGame(object sender, System.EventArgs e)
+        {
+            CCGameView gameview = sender as CCGameView;
+            if(gameview != null)
+            {
+                var contentSearchPaths = new List<string>() { "Fonts", "Sounds" };
+                CCSize viewsize = gameview.ViewSize;
+                int w = 768;
+                int h = 1024;
+
+                gameview.DesignResolution = new CCSizeI(w, h);
+
+                if(w < viewsize.Width)
+                {
+                    contentSearchPaths.Add("Images/Hd");
+                    CCSprite.DefaultTexelToContentSizeRatio = 2.0f;
+                }
+                else
+                {
+                    contentSearchPaths.Add("Images/ld");
+                    CCSprite.DefaultTexelToContentSizeRatio = 1.0f;
+                }
+
+                gameview.ContentManager.SearchPaths = contentSearchPaths;
+
+                CCScene gameScene = new CCScene(gameview);
+
+                gameScene.AddLayer(new GameLayer());
+                gameview.RunWithScene(gameScene);
+            }
         }
     }
 }
