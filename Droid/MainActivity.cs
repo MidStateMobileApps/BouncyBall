d3cd1c64db909d21a3f39328898f2db238e06483
Stilson <14586012@mstc.edu>
2018-01-30 19:03:55 -0600

    Added game scene and layer

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -3,6 +3,7 @@ using Android.Widget;
 using Android.OS;
 using Android;
 using CocosSharp;
+using System.Collections.Generic;
 
 namespace BouncyBall.Droid
 {
@@ -18,11 +19,45 @@ namespace BouncyBall.Droid
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
+            CCGameView gameView = (CCGameView)FindViewById(Resource.Id.GameView);
+            gameView.ViewCreated += LoadGame;
+
+
             // Get our button from the layout resource,
             // and attach an event to it
             //Button button = FindViewById<Button>(Resource.Id.myButton);
 
             //button.Click += delegate { button.Text = $"{count++} clicks!"; };
+        }
+
+        private void LoadGame(object sender, System.EventArgs e)
+        {
+            CCGameView gameView = sender as CCGameView;
+            if ( gameView != null)
+            {
+                var contentSearchPaths = new List<string>() { "Fonts", "Sounds" };
+                CCSizeI viewSize = gameView.ViewSize;
+                int w = 768;
+                int h = 1024;
+
+                gameView.DesignResolution = new CCSizeI(w, h);
+                if ( w < viewSize.Width)
+                {
+                    contentSearchPaths.Add("Images/Hd");
+                    CCSprite.DefaultTexelToContentSizeRatio = 2.0f;
+                }
+                else
+                {
+                    contentSearchPaths.Add("Images/Ld");
+                    CCSprite.DefaultTexelToContentSizeRatio = 1.0f;
+                }
+
+                gameView.ContentManager.SearchPaths = contentSearchPaths;
+                CCScene gameScene = new CCScene(gameView);
+
+                gameScene.AddLayer(new GameLayer());
+                gameView.RunWithScene(gameScene);
+            }
         }
     }
 }
