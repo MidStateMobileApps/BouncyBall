d34ce5c6ee577f0ea34530d886d5051e1ad7a0c9
Wergin <15952624@mstc.edu>
2018-02-06 18:47:16 -0600

    Added materials to BouncyBall

---
+++ BouncyBall/GameLayer.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CocosSharp;
+
+namespace BouncyBall
+{
+    public class GameLayer : CCLayerColor
+    {
+        CCSprite paddleSprite;
+        CCSprite ballSprite;
+        CCLabel scoreLabel;
+        CCLabel playLabel;
+
+        float ballXVelocity;
+        float ballYVelocity;
+
+        const float gravity = 140;
+        int levelMultiplier = 1;
+
+        int score = 0;
+
+        public GameLayer() : base(CCColor4B.Black)
+        {
+            paddleSprite = new CCSprite("paddle");
+            paddleSprite.PositionX = 100;
+            paddleSprite.PositionY = 100;
+            AddChild(paddleSprite);
+
+            ballSprite = new CCSprite("ball");
+            ballSprite.PositionX = 320;
+            ballSprite.PositionY = 600;
+            AddChild(ballSprite);
+
+            scoreLabel = new CCLabel("Score: 0", "Arial", 70, CCLabelFormat.SystemFont);
+            scoreLabel.PositionX = 50;
+            scoreLabel.PositionY = 1000;
+            scoreLabel.AnchorPoint = CCPoint.AnchorUpperLeft;
+            AddChild(scoreLabel);
+
+            playLabel = new CCLabel("Play Again?", "Chalkduster", 70);
+            playLabel.PositionX = 750;
+            playLabel.PositionY = 100;
+            playLabel.AnchorPoint = CCPoint.AnchorLowerRight;            
+
+            Schedule(RunGameLogic);
+        }
+
+        private void RunGameLogic(float frameTimeInSeconds)
+        {
+            ballYVelocity += frameTimeInSeconds * (-gravity * levelMultiplier);
+            ballSprite.PositionX += ballXVelocity * frameTimeInSeconds;
+            ballSprite.PositionY += ballYVelocity * frameTimeInSeconds;
+
+            bool doesBallOverlapPaddle = ballSprite.BoundingBoxTransformedToParent.IntersectsRect(paddleSprite.BoundingBoxTransformedToParent);
+            bool isMovingDownwards = ballYVelocity < 0;
+            bool isBallBelowPaddle = ballSprite.BoundingBoxTransformedToParent.MaxY < paddleSprite.BoundingBoxTransformedToParent.MidY;
+
+            if (isBallBelowPaddle)
+            {
+                ResetGame();
+                return;
+            }
+            if (doesBallOverlapPaddle && isMovingDownwards)
+            {
+                ballYVelocity *= -1;
+                const float minXVelocity = -300;
+                const float maxXVelocity = 300;
+                ballXVelocity = CCRandom.GetRandomFloat(minXVelocity, maxXVelocity);
+                score++;
+                scoreLabel.Text = "Score: " + score;
+            }
+            float ballRight = ballSprite.BoundingBoxTransformedToParent.MaxX;
+            float ballLeft = ballSprite.BoundingBoxTransformedToParent.MinX;
+
+            float screenRight = VisibleBoundsWorldspace.MaxX;
+            float screenLeft = VisibleBoundsWorldspace.MinX;
+
+            bool shouldReflectXVelocity = (ballRight > screenRight && ballXVelocity > 0) || (ballLeft < screenLeft && ballXVelocity < 0);
+
+            if (shouldReflectXVelocity)
+            {
+                ballXVelocity *= -1;
+            }
+        }
+
+        private void ResetGame()
+        {
+            StopAllActions();
+            Unschedule(RunGameLogic);
+
+            paddleSprite.PositionX = 100;
+            paddleSprite.PositionY = 100;
+
+            ballSprite.PositionX = 320;
+            ballSprite.PositionY = 600;
+
+            AddChild(playLabel);
+            CreateTouchListener();
+        }
+
+        private void CreateTouchListener()
+        {
+            var touchListener = new CCEventListenerTouchAllAtOnce();
+            touchListener.OnTouchesBegan = TouchesBegan;
+            AddEventListener(touchListener);
+        }
+
+        private void TouchesBegan(List<CCTouch> touches, CCEvent touchEvent)
+        {
+            foreach(var touch in touches)
+            {
+                if (playLabel.BoundingBoxTransformedToWorld.ContainsPoint(touch.Location))
+                {
+                    score = 0;
+                    levelMultiplier++;
+                    Schedule(RunGameLogic);
+                    RemoveChild(playLabel);
+                }
+            }
+        }
+
+        protected override void AddedToScene()
+        {
+            base.AddedToScene();
+            CCRect bounds = VisibleBoundsWorldspace;
+            var touchListener = new CCEventListenerTouchAllAtOnce();
+            touchListener.OnTouchesEnded = OnTouchesEnded;
+            touchListener.OnTouchesMoved = OnTouchesMoved;
+            AddEventListener(touchListener, this);
+        }
+
+        private void OnTouchesMoved(List<CCTouch> touches, CCEvent touchEvent)
+        {
+            var locationOnScreen = touches[0].Location;
+            paddleSprite.PositionX = locationOnScreen.X;
+        }
+
+        private void OnTouchesEnded(List<CCTouch> touches, CCEvent touchEvent)
+        {
+        }
+    }
+}
